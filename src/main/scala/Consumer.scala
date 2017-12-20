a42b447a9f947683bc1edf747410b9b05175c9fd
Dollyg <dollyg@thoughtworks.com>
2017-12-20 19:05:10 +0530

    Querrying one message before latest for the new subscribers to a running topic

--- src/main/scala/Consumer.scala
+++ src/main/scala/Consumer.scala
@@ -2,6 +2,9 @@ import java.util
 
 import org.apache.kafka.clients.consumer.KafkaConsumer
 import java.util.Properties
+
+import org.apache.kafka.common.TopicPartition
+
 import scala.collection.JavaConverters._
 
 object Consumer extends App {
@@ -18,8 +21,21 @@ object Consumer extends App {
   val topicsToSubscribe = new util.ArrayList[String]
   topicsToSubscribe.add(TOPIC)
   consumer.subscribe(topicsToSubscribe)
+  consumer.poll(0) //polling as subscribe is lazy
 
-  while(true) {
+
+  val topicPartitions = new util.ArrayList[TopicPartition]
+  val topicPartition = new TopicPartition("test", 0)
+  topicPartitions.add(topicPartition)
+
+  val endoffsets = consumer.endOffsets(topicPartitions)
+  println("End offsets=", endoffsets)
+  val lastOffset = endoffsets.get(topicPartition)
+  println("Last offset=", lastOffset)
+
+  consumer.seek(topicPartition, lastOffset - 1) //get one message before latest
+
+  while (true) {
     println("Before polling")
     val records = consumer.poll(1000)
     println("Polled")
