5119046dff569aeb073143e596cc98cc45992031
Dollyg <dollyg@thoughtworks.com>
2017-12-20 19:36:06 +0530

    unsubscribing a topic

--- src/main/scala/Consumer.scala
+++ src/main/scala/Consumer.scala
@@ -11,36 +11,46 @@ object Consumer extends App {
 
   val props = new Properties()
   props.put("bootstrap.servers", "localhost:9092")
-  props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
-  props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+  props.put("key.deserializer",
+    "org.apache.kafka.common.serialization.StringDeserializer")
+  props.put("value.deserializer",
+    "org.apache.kafka.common.serialization.StringDeserializer")
   props.put("group.id", "G_one")
   props.put("auto.offset.reset", "earliest")
 
-  val TOPIC = "test"
+  val TOPIC1 = "test1"
+  val TOPIC2 = "test2"
   val consumer = new KafkaConsumer[String, String](props)
-  val topicsToSubscribe = new util.ArrayList[String]
-  topicsToSubscribe.add(TOPIC)
-  consumer.subscribe(topicsToSubscribe)
+
+  val topicsToSubscribe = List(TOPIC1, TOPIC2)
+  consumer.subscribe(topicsToSubscribe.asJava)
   consumer.poll(0) //polling as subscribe is lazy
 
-
-  val topicPartitions = new util.ArrayList[TopicPartition]
-  val topicPartition = new TopicPartition("test", 0)
-  topicPartitions.add(topicPartition)
+  val topicPartition1 = new TopicPartition(TOPIC1, 0)
+  val topicPartitions = List(topicPartition1).asJava
 
   val endoffsets = consumer.endOffsets(topicPartitions)
   println("End offsets=", endoffsets)
-  val lastOffset = endoffsets.get(topicPartition)
+  val lastOffset = endoffsets.get(topicPartition1)
   println("Last offset=", lastOffset)
 
-  consumer.seek(topicPartition, lastOffset - 1) //get one message before latest
+  consumer.seek(topicPartition1, lastOffset - 1) //get one message before latest
 
-  while (true) {
+  for (i <- 1 to 50) {
     println("Before polling")
     val records = consumer.poll(1000)
     println("Polled")
     for (record <- records.asScala) {
       println("Consumed Record=", record)
+    }
+
+    if (i == 25) {
+      println("Topics before=",consumer.subscription())
+      val topicsToUnsubscribe = List(TOPIC2)
+      val newTopicsToSubscribe = topicsToSubscribe.diff(topicsToUnsubscribe)
+      consumer.subscribe(newTopicsToSubscribe.asJava)
+      consumer.poll(0) //polling as subscribe is lazy
+      println("Topics after=",consumer.subscription())
     }
   }
   println("Halting..")
